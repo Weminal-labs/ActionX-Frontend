d3374ffc03962a64e4422bdb9d17c840668ad73e
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-12 22:21:18 +0700

    refactor: Improve readability and handle URL decoding in CurrentPath component

---
+++ src/app/(action)/action/[...action-api]/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useEffect, useState } from "react";
+import { ActionLayout } from "@/components/ActionLayout";
+import { mapApiResponseToLayoutProps } from "@/utils/mappingUtils";
+import { usePathname } from "next/navigation";
+
+const CurrentPath = () => {
+  const pathname = usePathname();
+  const [apiAction, setApiAction] = useState("");
+  const [layoutProps, setLayoutProps] = useState<any>(null);
+
+  useEffect(() => {
+    const parts = pathname.split("api-action=");
+    if (parts.length > 1) {
+      const decodedPath = decodeURIComponent(parts[1]);
+      setApiAction(decodedPath);
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    const fetchApiData = async () => {
+      if (apiAction) {
+        try {
+          const response = await fetch(apiAction);
+          console.log("apiAction", apiAction);
+          console.log("response", response);
+          const data = await response.json();
+          console.log("data", data);
+          const mappedProps = mapApiResponseToLayoutProps(data);
+          setLayoutProps(mappedProps);
+        } catch (error) {
+          console.error("Error fetching API data:", error);
+        }
+      }
+    };
+
+    fetchApiData();
+  }, [apiAction]);
+
+  if (!layoutProps) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main className="flex h-screen items-center justify-center pt-4 lg:pt-0 dark:bg-black">
+      <div className="w-full max-w-md">
+        <ActionLayout {...layoutProps} />
+      </div>
+    </main>
+  );
+};
+
+export default CurrentPath;
