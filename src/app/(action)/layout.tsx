3f85aabe157b8e7279af74406ca21858cc97e7c4
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-20 15:21:06 +0700

    refactor: Add AutoConnectProvider and WalletProvider to layout component

--- src/app/(action)/layout.tsx
+++ src/app/(action)/layout.tsx
@@ -1,6 +1,9 @@
+import { AutoConnectProvider } from "@/components/AutoConnectProvider";
 import FooterActionpage from "@/components/footer/footer-actionpage";
 import HeaderActionPage from "@/components/header/header-actionpage";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import { WalletProvider } from "@/components/WalletProvider";
 
 export default function layout({
   children,
@@ -15,9 +18,14 @@ export default function layout({
         enableSystem
         disableTransitionOnChange
       >
-        <HeaderActionPage />
-        <div className="flex min-h-screen flex-col">{children}</div>
-        <FooterActionpage />
+        <AutoConnectProvider>
+          <WalletProvider>
+            <HeaderActionPage />
+            <div className="flex min-h-screen flex-col">{children}</div>
+            <FooterActionpage />
+            <Toaster />
+          </WalletProvider>
+        </AutoConnectProvider>
       </ThemeProvider>
     </div>
   );
