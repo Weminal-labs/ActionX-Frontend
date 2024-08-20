305cb6a7a34e24d05e128206d8a7ab30ec2dda16
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-20 15:21:45 +0700

    refactor: Update mappingUtils.ts to handle action clicks and fetch API data

--- src/utils/mappingUtils.ts
+++ src/utils/mappingUtils.ts
@@ -1,5 +1,6 @@
 import { BaseButtonProps } from "@/components/ui/inputs/types";
 import { LayoutProps } from "@/components/ActionLayout";
+import { aptosClient } from "@/utils";
 
 // Define the types for action with parameters
 export interface ActionWithParameters {
@@ -32,8 +33,59 @@ export const createButton = (
   action: ActionWithParameters
 ): BaseButtonProps => ({
   text: action.label,
-  onClick: () => window.open(action.href, "_blank"),
+  onClick: () => handleActionClick(action),
 });
+
+// Function to handle action clicks
+const handleActionClick = async (action: Action) => {
+  try {
+    let url = action.href;
+
+    // If the action has parameters, prompt user for input
+    if (isActionWithParameters(action)) {
+      const params = action.parameters.reduce((acc: any, param) => {
+        if (param.required) {
+          const value = prompt(param.label);
+          if (!value) {
+            alert(`The ${param.label} is required.`);
+            return;
+          }
+          acc[param.name] = encodeURIComponent(value);
+        }
+        return acc;
+      }, {});
+
+      // Replace placeholders in URL with user input
+      Object.keys(params).forEach((key) => {
+        url = url.replace(`{${key}}`, params[key]);
+      });
+    }
+
+    // Perform fetch request
+    const response = await fetch(url, {
+      method: "POST", // Change to "GET" if needed
+      headers: {
+        "Content-Type": "application/json",
+        // Add other headers if necessary
+      },
+      // If necessary, include body in request
+      // body: JSON.stringify({ toAddress: "address" })
+    });
+
+    const result = await response.json();
+    console.log(result);
+    const { transaction, message } = result;
+    console.log(transaction);
+
+    // Use signAndSubmitTransaction to sign and submit the transaction
+    // const pendingTransaction = await signAndSubmitTransaction(transaction);
+    // await aptosClient(network).waitForTransaction({
+    //   transactionHash: pendingTransaction.hash,
+    // });
+  } catch (error) {
+    console.error("Error handling action click:", error);
+  }
+};
 
 // Function to map API response to LayoutProps
 export const mapApiResponseToLayoutProps = (
@@ -60,7 +112,7 @@ export const mapApiResponseToLayoutProps = (
     buttons: actionsWithoutParameters.map((action: any) => ({
       label: action.label,
       text: action.label,
-      onClick: () => window.open(action.href, "_blank"),
+      onClick: () => handleActionClick(action),
     })),
     inputs: actionsWithParameters.flatMap((action: any) =>
       action.parameters.map((param: any) => ({
