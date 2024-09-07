08263a6492f165f03ec6ad029fac751299e29d44
lou1s vuong <lou1svuong.dev@gmail.com>
2024-09-07 20:41:53 +0700

    chore: Update landing page hero to include link to external site

--- src/containers/landing/hero.tsx
+++ src/containers/landing/hero.tsx
@@ -12,7 +12,9 @@ export const Hero = () => {
       <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] llg:h-[800px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
       <div className="container relative">
         <div className="flex items-center justify-center -mt-10">
-          <AnimatedGradientTextDemo />
+          <Link href={"https://x.com/ActionxApt"} target="_blank">
+            <AnimatedGradientTextDemo />
+          </Link>
         </div>
         <div className="flex justify-center mt-8 ">
           <div className="inline-flex relative">
