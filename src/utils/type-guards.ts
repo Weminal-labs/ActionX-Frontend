bf8bce5ce6754c468bc6556670031431c37bedd7
lou1s vuong <lou1svuong.dev@gmail.com>
2024-09-07 21:40:39 +0700

    refactor: Update type-guards.ts to import ActionPostResponse from the correct file

--- src/utils/type-guards.ts
+++ src/utils/type-guards.ts
@@ -1,9 +1,9 @@
-import type { ActionPostResponse } from '../api';
+import type { ActionPostResponse } from "../api/actions-spec";
 
 export const isSignTransactionError = (
-  data: { signature: string } | { error: string },
+  data: { signature: string } | { error: string }
 ): data is { error: string } => !!(data as any).error;
 
 export const isPostRequestError = (
-  data: ActionPostResponse | { error: string },
+  data: ActionPostResponse | { error: string }
 ): data is { error: string } => !!(data as any).error;
